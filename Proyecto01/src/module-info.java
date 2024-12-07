aa6db79e733484c33d6a831c29475fd21e679333
UnTalPoto <34370310+jorgevp2003@users.noreply.github.com>
2024-12-07 20:30:46 +0100

    Update module-info.java

--- Proyecto01/src/module-info.java
+++ Proyecto01/src/module-info.java
@@ -1,8 +1,8 @@
-/**
- * 
- */
-/**
- * 
- */
-module Proyecto01 {
+public class Prueba{
+	public static void public static void main(String[] args) {
+		
+		for(int i=0, i<20,i++){
+			System.out.println("hola" + i);
+		}
+	}
 }
